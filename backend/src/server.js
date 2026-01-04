0f6f3851172b9283c77266cfc69dc6a6e5b942f4
mrtnwilliam <mrtnwilliam41@gmail.com>
2026-01-04 15:38:19 +0200

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import {connectDB} from "./lib/db.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB()
+});
