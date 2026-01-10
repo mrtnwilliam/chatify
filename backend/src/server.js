c47c6f1a1377cdebc3295852a119282f7f39ef79
mrtnwilliam <mrtnwilliam41@gmail.com>
2026-01-10 19:42:58 +0200

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path"
+import cookieParser from "cookie-parser"
 
 import {connectDB} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
