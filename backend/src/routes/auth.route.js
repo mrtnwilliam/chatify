02e3e947d4ccbd13bf5a9b0f32e4664c9a316460
mrtnwilliam <mrtnwilliam41@gmail.com>
2026-01-13 09:44:47 +0200

    rate limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection)
+
 router.post("/signup", signup)
-router.post("/login",login)
-router.post("/logout",logout)
+router.post("/login", login)
+router.post("/logout", logout)
 
 router.put("/update-profile", protectRoute ,updateProfile)
 
