9308f1a94c9691942e7dd7dffdf4407996851b5d
mrtnwilliam <mrtnwilliam41@gmail.com>
2026-02-09 21:36:02 +0200

    fixed overflowing in mobile view

--- frontend/src/components/ChatHeader.tsx
+++ frontend/src/components/ChatHeader.tsx
@@ -21,18 +21,18 @@ function ChatHeader() {
   },[setSelectedUser])
 
   return (
-    <div className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-6 flex-1">
-      <div className="flex items-center space-x-3">
+    <div className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-4 sm:px-6 flex-1">
+      <div className="flex items-center gap-3 overflow-hidden">
         <div className={`avatar ${isOnline ? "online" : "offline"}`}>
-          <div className="w-12 rounded-full">
+          <div className="size-10 rounded-full relative">
             <img
               src={selectedUser?.profilePic || "/avatar.png"}
               alt={selectedUser?.fullName}
             />
           </div>
         </div>
-        <div>
-          <h3 className="text-slate-200 font-medium">{selectedUser?.fullName}</h3>
+        <div className="min-w-0">
+          <h3 className="text-slate-200 font-medium truncate">{selectedUser?.fullName}</h3>
           <p className="text-slate-400 text-sm">{isOnline ? "Online" : "Offline"}</p>
         </div>
       </div>
