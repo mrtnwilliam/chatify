22e356a43aa800951fe7bda292ed7a672abba3a3
mrtnwilliam <mrtnwilliam41@gmail.com>
2026-02-10 15:11:20 +0200

    test

--- frontend/src/components/ChatHeader.tsx
+++ frontend/src/components/ChatHeader.tsx
@@ -12,16 +12,17 @@ function ChatHeader() {
   useEffect(() => {
     const handleEscKey = (event: KeyboardEvent) => {
       if (event.key === "Escape") setSelectedUser(null);
-    }
+    };
 
-    window.addEventListener("keydown", handleEscKey)
+    window.addEventListener("keydown", handleEscKey);
 
     // cleanup function
-    return () => window.removeEventListener("keydown", handleEscKey)
-  },[setSelectedUser])
+    return () => window.removeEventListener("keydown", handleEscKey);
+  }, [setSelectedUser]);
 
   return (
-    <div className="p-2.5 flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 min-h-[64px] px-4 sm:px-6 w-full">
+    //    <div className="p-2.5 flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 min-h-[64px] px-4 sm:px-6 w-full">
+    <div className="p-2.5 flex justify-between items-center bg-red-500 min-h-[64px] px-4 w-full">
       <div className="flex items-center gap-3 overflow-hidden flex-1">
         <div className={`avatar ${isOnline ? "online" : "offline"}`}>
           <div className="size-10 rounded-full relative">
@@ -32,13 +33,20 @@ function ChatHeader() {
           </div>
         </div>
         <div className="min-w-0">
-          <h3 className="text-slate-200 font-medium truncate">{selectedUser?.fullName}</h3>
-          <p className="text-slate-400 text-sm">{isOnline ? "Online" : "Offline"}</p>
+          <h3 className="text-slate-200 font-medium truncate">
+            {selectedUser?.fullName}
+          </h3>
+          <p className="text-slate-400 text-sm">
+            {isOnline ? "Online" : "Offline"}
+          </p>
         </div>
       </div>
 
-      <button onClick={() => setSelectedUser(null)} className="flex-shrink-0 ml-2">
-        <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer"/>
+      <button
+        onClick={() => setSelectedUser(null)}
+        className="flex-shrink-0 ml-2"
+      >
+        <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
       </button>
     </div>
   );
